320eb82e4366cf46e1ebd605e5977cbe2304dc94
Saman Paidar <samanpaidar@gmail.com>
2017-08-25 13:36:06 +0200

    expense array and currency,date pipes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  expenses = [
+    {
+      date:'2016-01-01',
+      amount: 7.7 ,
+      category:'Food',
+      description:'Dinner'
+    },
+    {
+      date:'2016-01-01',
+      amount: 8.2 ,
+      category:'Ticket',
+      description:'Train ticket'
+    },
+    {
+      date:'2016-01-01',
+      amount: 5 ,
+      category:'Clothes',
+      description:'T-shirt'
+    }
+  ];
+  constructor(private navCtrl: NavController) {}
 
 }

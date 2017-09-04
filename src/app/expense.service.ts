205b007ee96c4333986fb1da402a18568cb9c7e0
Saman Paidar <samanpaidar@gmail.com>
2017-09-04 19:40:11 +0200

    post,put and delete implemented

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,37 +1,67 @@
 import uuidV4 from 'uuid/v4';
 import 'rxjs/add/operator/toPromise';
 import { Injectable} from '@angular/core'
-import { Http } from '@angular/http';
+import { Headers, Http, RequestOptions } from '@angular/http';
 import { Expense } from './expense.model';
 console.log(uuidV4());
 @Injectable()
 export class ExpenseService {
 	
+  //public apiUrl = 'http://localhost:9999';
 
   categories = ['Food', 'Travel', 'Other'];
   
   constructor( private http: Http){}
 
-  addExpense(expense: Expense): Promise<void> {
+  addExpense(expense: Expense): Promise<any> {
   	expense.id = uuidV4(); 
-  	return Promise.resolve();
+    const json = JSON.stringify(expense);
+
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    }); 
+
+    const options = new RequestOptions({
+      headers : headers
+    });
+
+  	return this.http.post('http://localhost:9999/expenses/', json, options)
+      .toPromise();
   }
+
   getExpense(expenseId: string): Promise<Expense> {
-    return Promise.resolve(null);
+    return this.http.get(`http://localhost:9999/expenses/${expenseId}`)
+      .toPromise()
+      .then(response => response.json());
+    
   }
+
   getExpenses(): Promise<Expense[]> {
   	return this.http.get('http://localhost:9999/expenses/')
   		.toPromise()
   		.then(response => response.json());
   	
   }
-  removeExpense(expenseId:string):Promise<void> {
-  	  	return Promise.resolve();
+
+  removeExpense(expenseId:string):Promise<any> {
+  	  	return this.http.delete(`http://localhost:9999/expenses/${expenseId}`)
+      .toPromise();
 
   }
 
- updateExpense(expense: Expense): Promise<void> {
-    return Promise.resolve();
+ updateExpense(expense: Expense): Promise<any> {
+    const json = JSON.stringify(expense);
+
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    }); 
+
+    const options = new RequestOptions({
+      headers : headers
+    });
+
+    return this.http.put(`http://localhost:9999/expenses/${expense.id}`, json, options)
+      .toPromise();
   }
 
 }

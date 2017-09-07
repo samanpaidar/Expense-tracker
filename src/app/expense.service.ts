537ab031a01180d54312f5c2671c8035511c3be5
Saman Paidar <samanpaidar@gmail.com>
2017-09-07 18:13:31 +0200

    authorization header

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,49 +1,43 @@
 import uuidV4 from 'uuid/v4';
 import { Injectable} from '@angular/core';
 import { Headers, Http, RequestOptions } from '@angular/http';
+import {AuthService} from './auth.service';
 import { Expense } from './expense.model';
 console.log(uuidV4());
 @Injectable()
 export class ExpenseService {
-	
-  //public apiUrl = 'http://localhost:9999';
+
+	private apiUrl = 'http://localhost:9999';
+
 
   categories = ['Food', 'Travel', 'Other'];
-  
-  constructor( private http: Http){}
+
+  constructor( private http: Http, private authService:AuthService){}
 
   addExpense(expense: Expense): Promise<any> {
-  	expense.id = uuidV4(); 
+  	expense.id = uuidV4();
     const json = JSON.stringify(expense);
 
-    const headers = new Headers({
-      'Content-Type': 'application/json'
-    }); 
-
-    const options = new RequestOptions({
-      headers : headers
-    });
-
-  	return this.http.post('http://localhost:9999/expenses/', json, options)
+  	return this.http.post(`${this.apiUrl}/expenses/`, json, this.requestOptions(true))
       .toPromise();
   }
 
   getExpense(expenseId: string): Promise<Expense> {
-    return this.http.get(`http://localhost:9999/expenses/${expenseId}`)
+    return this.http.get(`${this.apiUrl}/expenses/${expenseId}`,this.requestOptions())
       .toPromise()
       .then(response => response.json());
-    
+
   }
 
   getExpenses(): Promise<Expense[]> {
-  	return this.http.get('http://localhost:9999/expenses/')
+  	return this.http.get(`${this.apiUrl}/expenses/`,this.requestOptions())
   		.toPromise()
   		.then(response => response.json());
-  	
+
   }
 
   removeExpense(expenseId:string):Promise<any> {
-  	  	return this.http.delete(`http://localhost:9999/expenses/${expenseId}`)
+  	  	return this.http.delete(`${this.apiUrl}/expenses/${expenseId}`,this.requestOptions())
       .toPromise();
 
   }
@@ -51,16 +45,18 @@ export class ExpenseService {
  updateExpense(expense: Expense): Promise<any> {
     const json = JSON.stringify(expense);
 
-    const headers = new Headers({
-      'Content-Type': 'application/json'
-    }); 
-
-    const options = new RequestOptions({
-      headers : headers
-    });
-
-    return this.http.put(`http://localhost:9999/expenses/${expense.id}`, json, options)
+    return this.http.put(`${this.apiUrl}/expenses/${expense.id}`, json,this.requestOptions(true))
       .toPromise();
   }
 
+	private requestOptions(hasBody = false){
+		const headers = new Headers({
+			'Authorization':'bearer '+ this.authService.token,
+
+		});
+		if(hasBody){
+			headers.set('Content-Type', 'application/json');
+		}
+		return new RequestOptions({headers: headers});
+	}
 }
